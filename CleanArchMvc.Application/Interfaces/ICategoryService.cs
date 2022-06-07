6ccd3e85ea75031e8b29d0faacfd8d408ae62a4e
Queiroz <kalifor_dbel@hotmail.com>
2022-06-06 22:01:20 -0300

    Implmentação das classses de serviço e dto

---
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Application.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+
+        Task<CategoryDTO> GetById(int? id);
+
+        Task Add(CategoryDTO categoryDTO);
+
+        Task Update(CategoryDTO categoryDTO);
+
+        Task Remove(int? id);
+    }
+}
