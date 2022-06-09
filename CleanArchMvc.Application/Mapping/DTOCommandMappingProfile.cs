8d9bcd2e745b84175ea9668c6d1b7e62e24b5f56
Queiroz <kalifor_dbel@hotmail.com>
2022-06-09 19:13:00 -0300

    Implementando padrão CQRS e funcionalidades em products

---
+++ CleanArchMvc.Application/Mapping/DTOCommandMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTO;
+using CleanArchMvc.Application.Products.Commands;
+
+namespace CleanArchMvc.Application.Mapping
+{
+    public class DTOCommandMappingProfile : Profile
+    {
+        public DTOCommandMappingProfile()
+        {
+            CreateMap<ProductDTO, ProductCreateCommand>();
+            CreateMap<ProductDTO, ProductUpdateCommand>();
+        }
+    }
+}
