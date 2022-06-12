9ce0d22cffd99383beef2fc8bcab38fec3c8dba8
Queiroz <kalifor_dbel@hotmail.com>
2022-06-12 08:51:13 -0300

    Implementando view edit para category

--- CleanArchMvc.UI/Controllers/CategoriesController.cs
+++ CleanArchMvc.UI/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using CleanArchMvc.Application.DTO;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.UI.Controllers
@@ -36,6 +37,36 @@ namespace CleanArchMvc.UI.Controllers
             }
 
             return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var categoryDto = await _categoryService.GetById(id);
+            if (categoryDto == null)
+                return NotFound();
+            return View(categoryDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                { 
+                    await _categoryService.Update(categoryDTO);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryDTO);
         }
     }
 }
