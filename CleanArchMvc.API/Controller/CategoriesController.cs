24a56464ad7dae16d297143290e812b3de3b2f1f
Queiroz <kalifor_dbel@hotmail.com>
2022-06-25 20:49:33 -0300

    Implementação do crud de Categories finalizado

--- CleanArchMvc.API/Controller/CategoriesController.cs
+++ CleanArchMvc.API/Controller/CategoriesController.cs
@@ -27,16 +27,54 @@ namespace CleanArchMvc.API.Controller
             return Ok(categories);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetCategory")]
         public async Task<ActionResult<CategoryDTO>> Get(int id)
         {
-            var category = _categoryService.GetById(id);
+            var category = await _categoryService.GetById(id);
             if (category == null)
                 return NotFound("Category not found");
 
             return Ok(category);
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO == null)
+                return BadRequest("Invalid Data");
 
+            await _categoryService.Add(categoryDTO);
+
+            return new CreatedAtRouteResult("GetCategory",
+                                             new { id = categoryDTO.Id },
+                                             categoryDTO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] CategoryDTO categoryDTO)
+        {
+            if (id != categoryDTO.Id)
+                return BadRequest();
+
+            if (categoryDTO == null)
+                return BadRequest();
+
+            await _categoryService.Update(categoryDTO);
+
+            return Ok(categoryDTO);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id)
+        {
+            var category = _categoryService.GetById(id);
+
+            if (category == null)
+                return NotFound("Category not found");
+
+            await _categoryService.Remove(id);
+
+            return Ok(category);
+        }
     }
 }
