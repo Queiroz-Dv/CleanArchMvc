e27943fbc8f66ea8456e825f1bd36077caddc0bd
Queiroz <kalifor_dbel@hotmail.com>
2022-06-25 20:26:34 -0300

    Adicionando métodos da API

---
+++ CleanArchMvc.API/Controller/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchMvc.Application.DTO;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controller
+{
+    [Route("api/[controller]")] // Rota da api
+    [ApiController] // Permite habilitar outros serviços para api
+    public class CategoriesController : ControllerBase // Controller base omite o acesso a views
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+                return NotFound("Categories not found");
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = _categoryService.GetById(id);
+            if (category == null)
+                return NotFound("Category not found");
+
+            return Ok(category);
+        }
+
+
+    }
+}
